996b03230035a6966551ef4b5b13434fd093f67e
Vonrazel <bonyrazelmorales@gmail.com>
2023-10-27 13:43:56 -0700

    feat/select-active-players

--- hero.sql
+++ hero.sql
@@ -158,3 +158,8 @@ ADD item_price DECIMAL(10, 2);
 UPDATE public.hero
 SET is_active = false
 WHERE hero_id = 1;
+
+SELECT p.player_name, h.hero_name
+FROM public.player p
+JOIN public.hero h ON p.hero_id = h.hero_id
+WHERE h.is_active = true;
